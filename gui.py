05e0755d25c712f527b158fe6603b6720ecc1f5d
tedsnets <ted@tedsnets.com>
2024-03-25 13:26:05 -0500

    Implement the Complete and Exit buttons

--- gui.py
+++ gui.py
@@ -7,16 +7,24 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key="todolist",
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=("Helvetica", 20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values["todolist"])
+    try:
+        event, values = window.read()
+        print(1, event)
+        print(2, values)
+        print(3, values["todolist"])
+    except TypeError:
+        break
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -33,6 +41,15 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window["todolist"].update(values=functions.get_todos())
+        case "Complete":
+            todo_to_complete = values["todolist"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todolist"].update(values=functions.get_todos())
+            window["todo_input"].update(value="")
+        case "Exit":
+            break
         case "todolist":
             window["todo_input"].update(value=values["todolist"][0])
         case sg.WIN_CLOSED:
