704e9f40f7779a530538e10f071c2ea5dc62af4e
tedsnets <ted@tedsnets.com>
2024-03-26 14:16:30 -0500

    Implement "add new tasks" feature

--- web.py
+++ web.py
@@ -2,6 +2,12 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
@@ -10,5 +16,7 @@ st.subheader("This is my todo app.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Input box for new task to add", label_visibility="collapsed",
-              placeholder="Enter a Task to add.")
+st.text_input(label="Input box for new task to add",
+              label_visibility="collapsed",
+              placeholder="Enter a Task to add.",
+              on_change=add_todo, key="new_todo")
