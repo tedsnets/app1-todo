a732132aefdc37e59e26d3b904ac024c1ef632f2
tedsnets <ted@tedsnets.com>
2024-03-27 12:05:13 -0500

    Input clears after add, fixed bug, checkbox keys set to index

--- web.py
+++ web.py
@@ -1,22 +1,37 @@
 import streamlit as st
 import functions
+import os
+
+if not os.path.exists(r"files\todos.txt"):
+    os.makedirs("files")
+    with open(r"files\todos.txt", "w") as file:
+        pass
 
 todos = functions.get_todos()
+
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
-
+    st.session_state["new_todo"] = ""
 
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 
-
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=index)
+    if checkbox:
+        print(index, todo, todos[index])
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[index]
+        st.rerun()
 
 st.text_input(label="Input box for new task to add",
               label_visibility="collapsed",
               placeholder="Enter a Task to add.",
-              on_change=add_todo, key="new_todo")
+              on_change=add_todo, key="new_todo",)
+
+# st.session_state
